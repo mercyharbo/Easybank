a261262815592b7799f0e077d44786280a67465a
mercyharbo <damilare791@gmail.com>
2021-06-02 13:38:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items');
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
